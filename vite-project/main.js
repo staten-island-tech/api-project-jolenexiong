4170c2312effdcd579f5789f39afbae278a7f184
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2023-12-12 13:15:28 -0500

    ssss

--- vite-project/main.js
+++ vite-project/main.js
@@ -4,5 +4,16 @@ async function getData() {
   let result= await fetch ("https://southparkquotes.onrender.com/v1/quotes/search/cartman");
 let data = await result.json();
 console.log(data);
+DOMSelectors.quote.textContent=data.quote
 }
+
 getData();
+
+const DOMSelectors = {
+  quote: document.querySelector(".quote")
+}
+
+
+
+
+
