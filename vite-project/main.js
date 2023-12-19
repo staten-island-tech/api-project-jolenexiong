19bb688f3a86fb2a013d79a04d6896955278d0b9
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2023-12-19 13:07:38 -0500

    k

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,19 +1,24 @@
-import './style.css'
+
 
 const URL = 'https://southparkquotes.onrender.com/';
 
-async function getData(URL) {
-  const reponse = await fetch(URL);
-  console.log(reponse);
-//   let result= await fetch ("https://southparkquotes.onrender.com/v1/quotes/search/cartman");
-// let data = await result.json();
-// console.log(data);
-DOMSelectors.quote.textContent=data.quote;
+// async function getData(URL) {
+//   const reponse = await fetch(URL);
+//   console.log(reponse);
+// //   let result= await fetch ("https://southparkquotes.onrender.com/v1/quotes/search/cartman");
+// // let data = await result.json();
+// // console.log(data);
+// DOMSelectors.quote.textContent=data.quote;
+// }
+
+async function addQuote(URL) {
+  const response = await fetch(URL);
+  console.log(response);
+  const data = await response.json();
+  DOMSelectors.quote.textContent = data.quote;
 }
 
-
-
-getData();
+addQuote(URL);
 
 const DOMSelectors = {
   quote: document.querySelector(".quote")
