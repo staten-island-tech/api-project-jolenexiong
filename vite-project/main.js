51fccd6d2e6a95ee538cbc864e8dd66643286196
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2024-01-02 21:30:50 -0500

    ddd

--- vite-project/main.js
+++ vite-project/main.js
@@ -11,25 +11,35 @@ import "./css/style.css";
 // }
 const URL1 = 'https://southparkquotes.onrender.com/v1/quotes';
 
+const DOMSelectors = {
+  question: document.querySelector("#app h1"),
+  submit: document.querySelector(".submitbtn"),
+}
+
 async function addQuote(URL1) {
   const res = await fetch(URL1);
   console.log(res);
   const data = await res.json();
   console.log(data);
-  document.querySelector("h1").textContent = data.quote;
+  document.querySelector.question.textContent = data.quote;
 }
 
 addQuote(URL1);
 
+DOMSelectors.submit.addEventListener('click', function (event) {
+  event.preventDefault();
+const guess = document.getElementById('whosaid').value;
+const answer = document.querySelector("#app h1").textContent;
+const correct = document.querySelector('.check');
 
-// function check {
-//   guess: document.querySelector("#guess"),
-//   if (
-//      DOMSelectors.guess.value === data.quote 
-//   )
-  
-  
-// }
+if (guess()=== answer()) {
+  correct.textContent = 'Yass you did it!!!! you are amazing and beautiful';
+} else {
+  correct.textContent = 'wow you really flopped...';
+}
+});
+
+
 
 
 
